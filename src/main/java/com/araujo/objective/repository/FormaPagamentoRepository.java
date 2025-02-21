4d33fb812d523065849c8b30f42406917e44c697
brunoleitearaujo <bruno.araujo1989@gmail.com>
2025-02-21 15:30:59 -0300

    Ao iniciar api, carregar os dados da forma de pagamento.

---
+++ src/main/java/com/araujo/objective/repository/FormaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package com.araujo.objective.repository;
+
+import com.araujo.objective.entity.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+}
