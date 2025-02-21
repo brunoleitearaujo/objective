e7b5e51a275736719374606b0a7a39035f1ceaf8
brunoleitearaujo <bruno.araujo1989@gmail.com>
2025-02-21 14:54:38 -0300

    Criando api

---
+++ src/test/java/com/araujo/objective/ObjectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujo.objective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
