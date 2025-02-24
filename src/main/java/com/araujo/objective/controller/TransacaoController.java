d2b96ae49a4ac1b736c52327a4d65db551f13842
brunoleitearaujo <bruno.araujo1989@gmail.com>
2025-02-24 14:31:19 -0300

    Adicionando o swagger open api

--- src/main/java/com/araujo/objective/controller/TransacaoController.java
+++ src/main/java/com/araujo/objective/controller/TransacaoController.java
@@ -3,6 +3,9 @@ package com.araujo.objective.controller;
 import com.araujo.objective.controller.dto.ContaDTO;
 import com.araujo.objective.controller.dto.TransacaoDTO;
 import com.araujo.objective.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,13 @@ public class TransacaoController {
     }
 
     @PostMapping
+    @Operation(description = "Endpoint responsável por criar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "404", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<ContaDTO> createTransacao(@RequestBody @Valid TransacaoDTO transacaoDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.createTransacao(transacaoDTO));
     }
