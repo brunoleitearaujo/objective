a9da3111be1c0510f6ec9587d3ad13058e5a7413
brunoleitearaujo <bruno.araujo1989@gmail.com>
2025-02-24 01:03:35 -0300

    Criando endpoint de buscar conta

--- src/main/java/com/araujo/objective/Exception/ContaNaoEncontradaException.java
+++ src/main/java/com/araujo/objective/Exception/ContaNaoEncontradaException.java
@@ -7,7 +7,7 @@ public class ContaNaoEncontradaException extends ObjectiveException {
 
     @Override
     public ProblemDetail toProblemDetail() {
-        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+        var pb = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
 
         pb.setTitle("Conta não encontrada");
 
--- src/main/java/com/araujo/objective/controller/ContaController.java
+++ src/main/java/com/araujo/objective/controller/ContaController.java
@@ -5,6 +5,8 @@ import com.araujo.objective.service.ContaService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,10 @@ public class ContaController {
     @PostMapping
     public ResponseEntity<ContaDTO> createConta(@RequestBody @Valid ContaDTO contaDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(contaService.createConta(contaDTO));
+    }
+
+    @GetMapping("/{numeroConta}")
+    public ResponseEntity<ContaDTO> getConta(@PathVariable Long numeroConta) {
+        return ResponseEntity.ok().body(contaService.getConta(numeroConta));
     }
 }
